017ff57da938a26e040e416fc465c347be63ce81
Stefano Vincenzi <stevi@MacBook-Pro-di-Stefano-3.local>
2017-04-28 17:48:53 +0200

    manca: css,cancellare commenti(?),controlli su registrazione

--- js/registration.js
+++ js/registration.js
@@ -1,7 +1,7 @@
 window.onload = function() {
 
   var imgName = "";
-
+Dropzone.autoDiscover = false; //nuovo
   var dropz = new Dropzone("#mydropzone", {
     url: '../php/registration.php',
     paramName: 'file',
@@ -9,6 +9,8 @@ window.onload = function() {
     addRemoveLinks: true,
     autoProcessQueue: false,
     autoDiscover: false,
+    thumbnailWidth:50,
+    thumbnailHeight:50,
     previewsContainer: '#dropzonePreview',
     clickable: false,
 
@@ -36,4 +38,5 @@ window.onload = function() {
       })
     }
   })
+
 }
